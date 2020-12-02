39949d6d486fe3dbe7dc5b537239bd47262b31ea
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-12-02 20:49:22 +0200

    Added hypothesis function

--- conf.py
+++ conf.py
@@ -1,3 +1,14 @@
 TRAIN_PATH='/home/bekh/mnt/HDD/FCI/Level 4 - Sem1/Machine Learning/ass2/house_data.csv'
 featureCols=['sqft_living']
 labelCol=['price']
+
+import numpy as np
+
+def h(x, c):
+    '''
+    hypothesis function
+    :param x: numpy array representing feature vector
+    :param c: numpy array representing weights
+    :return: float value representing the prediction
+    '''
+    return np.transpose(c).dot(x)[0]
--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import conf
 import numpy as np
 from GradientDescent import GradientDescent
 
-def h():
-    print("Hello")
-
 if __name__ == "__main__":
     ds = Dataset(conf.TRAIN_PATH, conf.featureCols, conf.labelCol)
-    g = GradientDescent(0.1, ds, 10, h)
-    g.testing()
+    g = GradientDescent(0.1, ds, 10, conf.h)
     pass
