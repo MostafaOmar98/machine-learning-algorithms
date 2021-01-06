bc0629632f9db4489da7dbb73c992f3cf53241f3
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-06 16:21:25 +0200

    add Data_Wrapper to split training and testing data read data from file,labels and features cols and split the data

---
+++ Data_Wrapper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from DataSet import DataSet
+
+
+class Data:
+    training = None
+    testing = None
+
+    def __init__(self, path, featureCols, labelCol, trainSize):
+        '''
+        path is absolute
+        path is a csv file
+        '''
+        self.features = pd.read_csv(filepath_or_buffer=path, usecols=featureCols).to_numpy(dtype='float64')
+        self.labels = pd.read_csv(filepath_or_buffer=path, usecols=labelCol).to_numpy(dtype='float64')
+        self.labels = self.labels.flatten()
+        self.n = self.features.shape[1]  # Number of features with bias included
+        self.m = self.features.shape[0]
+        self.training = DataSet(self.features[:int(trainSize * self.m)], self.labels[:int(trainSize * self.m)], True,
+                                True)
+        self.testing = DataSet(self.features[int((1 - trainSize) * self.m):],
+                               self.labels[int((1 - trainSize) * self.m):], True, True)
