c200a033f0ffc8f91cff5ab6aa3517df9be10dfa
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-06 19:18:35 +0200

    init commit for vote conf,just reading data till now

---
+++ votes_conf.py
@@ -0,0 +1,14 @@
+
+TRAIN_PATH = './assets/house-votes-84.data.csv'
+TRAIN_SIZE = 0.8
+TEST_SIZE = 1 - TRAIN_SIZE
+featureCols = []
+for i in range(1, 17):
+    featureCols.append("c{0}".format(str(i)))
+for i in featureCols:
+    print(i)
+labelCol = ["target"]
+print(featureCols)
+preProc = False
+alpha = 0.01
+MAX_ITERATIONS = 500
