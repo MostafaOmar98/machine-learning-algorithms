3cb3134067f3df462a4ab56ee7b20d2e809334a3
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-07 16:53:47 +0200

    add filtering generic on column to split on unique values add run to test function so far

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from DataSet import DataSet
 
 
 # we add the absence by the giving the major vote to missing votes in dataset
@@ -13,3 +14,22 @@ def populate_absence(features: np.ndarray):
         for i in range(column.shape[0]):
             if column[i] == '?':
                 column[i] = winner
+
+
+#     it takes dataset and feature column index and filter to yes and no
+def filterOnFeature(wholeData: np.ndarray, feature: int):
+    uniqueValues = np.unique(wholeData[:, feature])
+    # filteredNo = wholeData[wholeData[:, feature] != 'y']
+    # filteredYes = wholeData[wholeData[:, feature] != 'n']
+    # return [filteredYes, filteredNo]
+    # return [map( lambda value : wholeData[:,feature]!=value,uniqueValues)]
+    return [wholeData[wholeData[:, feature] != value] for value in uniqueValues]
+
+
+def run(dataset: DataSet):
+    populate_absence(dataset.features)
+    dataset.mergeFeatureAndLabel()
+    for arr in filterOnFeature(dataset.wholeData, 0):
+        for row in arr:
+            print(row)
+            break
