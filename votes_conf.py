7c2033eea8f56ececaf78f083a73d71199f1d9ae
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-07 17:20:57 +0200

    remove redundant prints

--- votes_conf.py
+++ votes_conf.py
@@ -5,10 +5,7 @@ TEST_SIZE = 1 - TRAIN_SIZE
 featureCols = []
 for i in range(1, 17):
     featureCols.append("c{0}".format(str(i)))
-for i in featureCols:
-    print(i)
 labelCol = ["target"]
-print(featureCols)
 preProc = False
 addbias = False
 alpha = 0.01
