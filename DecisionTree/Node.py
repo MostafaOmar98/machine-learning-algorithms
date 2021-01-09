e960d0f511268552245f0ba01ab176f326b2c994
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-09 19:31:20 +0200

    add addchildren function and fixed setChildren

--- DecisionTree/Node.py
+++ DecisionTree/Node.py
@@ -10,8 +10,15 @@ class Node:
     result = None
 
     def __int__(self, *argv):
-        self.setChildren()
+        self.setChildren(argv)
+
+    def __init__(self, featureName):
+        self.featureName = featureName
+        self.children = []
 
     def setChildren(self, *args):
+        self.children = []
         for arg in args:
             self.children.append(arg)
+    def addToChildren(self, child: Node):
+        self.children.append(child)
