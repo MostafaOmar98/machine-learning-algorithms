67c3387d8f2ecc8e4a92fb3cd547c34df240af24
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-09 16:11:05 +0200

    add preprocess function to vote_conf

--- votes_conf.py
+++ votes_conf.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 TRAIN_PATH = './assets/house-votes-84.data.csv'
 TRAIN_SIZE = 0.8
@@ -10,3 +11,18 @@ preProc = False
 addbias = False
 alpha = 0.01
 MAX_ITERATIONS = 500
+
+
+
+# we add the absence by the giving the major vote to missing votes in dataset
+def preprocess(features: np.ndarray):
+    yes = 0
+    no = 0
+    for column in features.T:
+        unique, count = np.unique(column, return_counts=True)
+        ans = dict(zip(unique, count))
+        winner = 'y' if ans['y'] > ans['n'] else 'n'
+        for i in range(column.shape[0]):
+            if column[i] == '?':
+                column[i] = winner
+
