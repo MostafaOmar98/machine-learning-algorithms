2404798f976ac809cf29f4eada63d0919e09a183
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-10 19:48:02 +0200

    add getHeight and getSize

--- DecisionTree/Node.py
+++ DecisionTree/Node.py
@@ -19,5 +19,21 @@ class Node:
         self.children = []
         for arg in args:
             self.children.append(arg)
+
     def addToChildren(self, child: Node):
         self.children.append(child)
+
+    def getHeight(self,currentNode: Node, currentHeight):
+        if len(currentNode.children) == 0:
+            return currentHeight
+        mx = 0
+        for child in currentNode.children:
+            mx = max(mx, self.getHeight(child, currentHeight + 1))
+        return mx
+    def getSize(self,currentNode: Node):
+        if len(currentNode.children) == 0:
+            return 0
+        ret = 0
+        for child in currentNode.children:
+            ret = ret + 1 + self.getSize(child)
+        return ret
